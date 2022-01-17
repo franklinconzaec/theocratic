7c3f254f106cb551f1f7a4144bed49a524932c50
Franklin Conza-Apolo <me@franklinconza.com>
2022-01-17 15:54:12 -0500

    first interface

--- src/main/java/com/franklinconza/theocratic/TelephoneMonitoringView.java
+++ src/main/java/com/franklinconza/theocratic/TelephoneMonitoringView.java
@@ -1,12 +1,19 @@
 package com.franklinconza.theocratic;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
 
 @Component
 @ManagedBean
 @ViewScoped
-public class TelephoneMonitoringBean {
+public class TelephoneMonitoringView implements Serializable {
+
+    @Getter @Setter
+    private Integer numberPhones;
+
 }
