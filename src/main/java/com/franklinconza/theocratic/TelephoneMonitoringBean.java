f61e9fc8bf26804ffe1f20e3a8e0a587e1450228
Franklin Conza-Apolo <me@franklinconza.com>
2022-01-17 07:41:05 -0500

    first interface

---
+++ src/main/java/com/franklinconza/theocratic/TelephoneMonitoringBean.java
@@ -0,0 +1,12 @@
+package com.franklinconza.theocratic;
+
+import org.springframework.stereotype.Component;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@Component
+@ManagedBean
+@ViewScoped
+public class TelephoneMonitoringBean {
+}
---
+++ src/test/java/com/franklinconza/theocratic/TheocraticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franklinconza.theocratic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheocraticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
